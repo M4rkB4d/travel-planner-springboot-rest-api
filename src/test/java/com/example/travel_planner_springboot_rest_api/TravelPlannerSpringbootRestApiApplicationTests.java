a72b63b7d45ff855fc13a99b955cdaac6214fcfc
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2024-12-28 01:48:22 +0800

    add initial project setup

---
+++ src/test/java/com/example/travel_planner_springboot_rest_api/TravelPlannerSpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_planner_springboot_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerSpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
